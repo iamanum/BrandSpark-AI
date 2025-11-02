2701076a62323af39046a4cdd598281db0cc531f
iamanum <iamanummunir>
2025-11-02 20:26:01 +0500

    Fix TypeScript import in theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,23 +1,20 @@
-// components/theme-provider.tsx
 "use client";
 
 import * as React from "react";
-// 🎨 15+ YR EXPERT FIX: Hum props ko seedha 'next-themes' se import kar rahe hain
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({
   children,
   ...props
-}: ThemeProviderProps) {
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return (
     <NextThemesProvider
-      attribute="class" // <-- Yeh Tailwind CSS (darkMode: "class") ke liye zaroori hai
-      defaultTheme="light" // Default theme set kar rahe hain
-      enableSystem={false} // Hum OS ki setting nahi, user ki setting istemal karenge
+      attribute="class" // Tailwind CSS (darkMode: "class") ke liye zaroori
+      defaultTheme="light" // Default theme
+      enableSystem={false} // OS ki setting nahi, user ki setting
       {...props}
     >
       {children}
     </NextThemesProvider>
   );
 }
-
