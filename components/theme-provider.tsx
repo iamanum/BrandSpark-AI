3f77ac196bdf15bdda249574ab5d62c8bd821b19
iamanum <iamanummunir>
2025-11-02 19:50:39 +0500

    fix: final commit - implemented correct theme-provider props

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,6 +1,8 @@
+// components/theme-provider.tsx
 "use client";
 
 import * as React from "react";
+// 🎨 15+ YR EXPERT FIX: Hum props ko seedha 'next-themes' se import kar rahe hain
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({
@@ -9,12 +11,13 @@ export function ThemeProvider({
 }: ThemeProviderProps) {
   return (
     <NextThemesProvider
-      attribute="class"
-      defaultTheme="light"
-      enableSystem={false}
+      attribute="class" // <-- Yeh Tailwind CSS (darkMode: "class") ke liye zaroori hai
+      defaultTheme="light" // Default theme set kar rahe hain
+      enableSystem={false} // Hum OS ki setting nahi, user ki setting istemal karenge
       {...props}
     >
       {children}
     </NextThemesProvider>
   );
 }
+
