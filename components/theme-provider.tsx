ab6440679e72a666aefe6abf295fcef75c5e14b0
iamanum <iamanummunir>
2025-11-02 20:34:55 +0500

    Remove invalid ThemeProviderProps import and fix TypeScript

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,17 +3,21 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-// Use inferred props instead of ThemeProviderProps
+/**
+ * A wrapper for next-themes ThemeProvider
+ * Fully typed using React.ComponentProps
+ * Supports Tailwind CSS dark mode (class strategy)
+ */
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
   return (
     <NextThemesProvider
-      attribute="class"  // Tailwind CSS dark mode
-      defaultTheme="light"
-      enableSystem={false}
-      {...props}
+      attribute="class"      // Required for Tailwind dark mode (darkMode: "class")
+      defaultTheme="light"    // Default theme when no preference is set
+      enableSystem={true}     // Set true if you want to respect user's OS theme
+      {...props}              // Spread any extra props you pass
     >
       {children}
     </NextThemesProvider>
