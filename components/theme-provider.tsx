a5b15ef231e78c97297ea5daa5ff58b39a473a3a
iamanum <iamanummunir>
2025-11-02 20:28:55 +0500

    Fix next-themes TypeScript import issue

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,15 +3,16 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+// Use React.ComponentProps to infer the props instead of importing ThemeProviderProps
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
   return (
     <NextThemesProvider
-      attribute="class" // Tailwind CSS (darkMode: "class") ke liye zaroori
-      defaultTheme="light" // Default theme
-      enableSystem={false} // OS ki setting nahi, user ki setting
+      attribute="class"  // Tailwind CSS dark mode
+      defaultTheme="light"
+      enableSystem={false}
       {...props}
     >
       {children}
